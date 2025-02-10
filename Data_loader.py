Scaling issue resolved Fusion module needs cleaning up Scaling fix needs to be implemented

Data_loader.py
@@ -1,10 +1,53 @@
-from hypso.hypso1 import Hypso1
+import hypso
+import hypso.calibration
 from utilities import Get_path
+import cv2
+import numpy as np
+import os
+import json
 
 # Initialize a Path object
-file_path, name = Get_path()
+file_path, name = Get_path("L1A file")
+#file_path = "data\\bluenile_2025-01-25T08-23-16Z-l1a.nc"
+meta_path = str(file_path).replace("l1a.nc", "meta.json")
 
-HSI_file = Hypso1(file_path)
-HSI_file._run_calibration()
-HSI_file._run_geometry()
-HSI_file.write_l1b_nc_file()
+with open(meta_path, 'r') as file:
+    metadata = json.load(file)
+
+# Extract the values (adjust keys if nested differently)
+gsd_along = metadata.get('gsd_along')
+gsd_across = metadata.get('gsd_across')
+
+sat_obj = hypso.Hypso1(file_path, verbose=True)
+sat_obj.generate_l1b_cube() #Note that filepaths on lines 905 and 913 are changed to hypso2 files
+#sat_obj.generate_geometry()
+sat_obj.write_l1b_nc_file(overwrite=True)
+
+#Nominal: 956, Full: 1092
+
+R, G, B = np.sum(sat_obj.l1b_cube[:,:,58:61],axis=2), np.sum(sat_obj.l1b_cube[:,:,69:72],axis=2), np.sum(sat_obj.l1b_cube[:,:,87:91],axis=2)
+rgb = np.stack([R,G,B], axis=-1)
+
+normalized = cv2.normalize(rgb, None, 0, 255, cv2.NORM_MINMAX)
+normalized = np.uint8(normalized)  # Convert to uint8 for saving
+
+bgr_image = cv2.cvtColor(normalized, cv2.COLOR_RGB2BGR)
+
+dx = gsd_along/gsd_across
+dy = 1.0
+height, width = bgr_image.shape[:2]
+
+if dx < dy:
+    scale_factor = dy / dx
+    new_width = int(width * scale_factor)
+    new_height = height
+else:
+    scale_factor = dx / dy
+    new_width = width
+    new_height = int(height * scale_factor)
+    
+corrected_image = cv2.resize(bgr_image, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
+
+rgb = np.asarray(corrected_image)[:,:,::-1] #Scaled datacube
+
+cv2.imwrite("slice.png", corrected_image)  # Save as JPG or PNG

fusion_module.py
@@ -7,28 +7,37 @@ from CNMF import CNMF, Get_VCA
 import loader as ld
 import time
 import CPPA as ppa
-
+from spatial_transform import full_transform, get_pixels
+import json
+import cv2
 
 class Fusion:
-        def __init__(self, name = ""):
+        def __init__(self, name = ""): #THIS IS A BAD INIT FUNCTION
                 if name == "":
-                        self.data_string, self.name = util.Get_path()
+                        self.data_string, self.name = util.Get_path("l1b file")
                 else:
                         self.name = name
                         self.data_string = Path(f"C:/Users/phili/Desktop/Image_fusion/data/{self.name}.nc")
                 self.read_config()
-                self.pix_coords = [self.x_start,self.x_end,self.y_start,self.y_end]
-                self.loops = (self.inner_loops, self.outer_loops)
                 self.full_arr = ld.load_l1b_cube(self.data_string)
-                offset = 0 #ONLY USED FOR TESTING SPATIAL ERROR
+                rgb_path = str(self.data_string).replace("16Z-l1b.nc", "14.png")
+                self.rgb_img = cv2.normalize(cv2.imread(rgb_path, cv2.IMREAD_GRAYSCALE), None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+                meta_path = str(self.data_string).replace("l1a.nc", "meta.json")
+
+                with open(meta_path, 'r') as file:
+                        metadata = json.load(file)
+                dx = metadata.get('gsd_along')/metadata.get('gsd_across')
+                self.flip = True
+                self.active_area, self.transform = full_transform(self.rgb_img, self.full_arr)
+                self.full_arr = self.full_arr[self.active_area[0]:self.active_area[1],self.active_area[2]:self.active_area[3]]
+                self.loops = (self.inner_loops, self.outer_loops)
                 if self.remove_darkest:
                         self.arr = Normalize(util.remove_darkest(self.full_arr[self.x_start:self.x_end,self.y_start:self.y_end,:]), min=1E-6, max=1.0)
-                        self.offset_arr = Normalize(util.remove_darkest(self.full_arr[self.x_start+offset:self.x_end+offset,self.y_start+offset:self.y_end+offset,:]), min=1E-6, max=1.0)
+                        self.offset_arr = Normalize(util.remove_darkest(self.full_arr[self.x_start:self.x_end,self.y_start:self.y_end,:]), min=1E-6, max=1.0)
                 else:
                         self.arr = Normalize(self.full_arr[self.x_start:self.x_end,self.y_start:self.y_end,:], min=1E-6, max=1.0)
-                        self.offset_arr = Normalize(self.full_arr[self.x_start+offset:self.x_end+offset,self.y_start+offset:self.y_end+offset,:], min=1E-6, max=1.0)
-                self.size = (self.x_end-self.x_start,self.y_end-self.y_start)
-                self.lowres_downsampled = util.Downsample(self.offset_arr, sigma=self.sigma, downsampling_factor=self.downsample_factor)
+                        self.offset_arr = Normalize(self.full_arr[self.x_start:self.x_end,self.y_start:self.y_end,:], min=1E-6, max=1.0)
+                self.size = (self.active_area[1]-self.active_area[0],self.active_area[3]-self.active_area[2])
                 if self.type == "CNMF":
                         self.w_init = Get_VCA(self.lowres_downsampled, self.endmember_n)
                         self.h_init = np.ones(shape=(self.endmember_n, self.lowres_downsampled.shape[0]*self.lowres_downsampled.shape[1]))

spatial_transform.py
@@ -2,17 +2,17 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-
-def align_and_overlay(hsi_path, rgb_path, output_path): #TODO rewrite this to crop and recalculate transform to only include active area
-    hsi_img = cv2.imread(hsi_path, cv2.IMREAD_GRAYSCALE)
-    rgb_img = cv2.imread(rgb_path, cv2.IMREAD_GRAYSCALE)
+import utilities as util
+def align_and_overlay(hsi_img, rgb_img, output_path): #TODO rewrite this to crop and recalculate transform to only include active area
     rgb_img = cv2.GaussianBlur(rgb_img, (15, 15), 0)
+    #rgb_img = rgb_img[::3,::3]
+    rgb_img = cv2.flip(rgb_img, 1)
 
     cv2.imwrite(f"{output_path}rgb.png", rgb_img)
     cv2.imwrite(f"{output_path}hsi.png", hsi_img)
     
     # Initialize SIFT detector - parameters are almost definitely sub-optimal
-    sift = cv2.SIFT_create(nfeatures=5000, contrastThreshold=0.02, edgeThreshold=20, sigma=2)
+    sift = cv2.SIFT_create(nfeatures=50000, contrastThreshold=0.015, edgeThreshold=40, sigma=2)
 
     # Detect keypoints and descriptors
     kp_hsi, des_hsi = sift.detectAndCompute(hsi_img, None)
@@ -25,20 +25,20 @@ def align_and_overlay(hsi_path, rgb_path, output_path): #TODO rewrite this to cr
     # Apply Lowe's ratio test to filter matches
     good_matches = []
     for m, n in matches:
-        if m.distance < 0.7 * n.distance:
+        if m.distance < 0.75 * n.distance:
             good_matches.append(m)
 
     img_matches = cv2.drawMatches(hsi_img, kp_hsi, rgb_img, kp_rgb, good_matches[:50], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(20, 10))
     plt.imshow(img_matches)
     
     plt.savefig(f"{output_path}matches.png")
     plt.close()
 
     # Extract the matching keypoints
-    src_pts = np.float32([kp_rgb[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp_hsi[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    src_pts = np.float32([kp_hsi[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    dst_pts = np.float32([kp_rgb[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, ransacReprojThreshold=5,confidence=0.995, maxIters=5000)
     
@@ -50,13 +50,13 @@ def align_and_overlay(hsi_path, rgb_path, output_path): #TODO rewrite this to cr
     #print(f"{np.sum(inliers)} inliers and {np.sum(outliers)} outliers")
 
     # Transform
-    aligned_rgb = cv2.warpPerspective(rgb_img, M, (hsi_img.shape[1], hsi_img.shape[0]))
+    aligned_hsi = cv2.warpPerspective(hsi_img, M, (rgb_img.shape[1], rgb_img.shape[0]))
 
     # Save the aligned image
-    cv2.imwrite(f"{output_path}aligned_rgb.png", aligned_rgb)
+    cv2.imwrite(f"{output_path}aligned_hsi.png", aligned_hsi)
 
     # blend images
-    overlay_img = cv2.addWeighted(hsi_img, 0.5, aligned_rgb, 0.5, 0)
+    overlay_img = cv2.addWeighted(rgb_img, 0.5, aligned_hsi, 0.5, 0)
     
     cv2.imwrite(f"{output_path}overlay.png", overlay_img)
     return M
@@ -74,42 +74,135 @@ def generate_full_spatial(hsi_dim,rgb_dim, transformation):
     #Indicates that RGB pixel (250,250) is spatially equivelent to HSI pixel (2045, 145)
     #For the moment these are being brute forced into specific pixels using Int() THIS SHOULD BE FIXED
     Dest = np.zeros(shape=(rgb_dim[0], rgb_dim[1], 2), dtype=np.int16)
-    n_pixel = 24
-    Source = np.full(shape=(hsi_dim[0], hsi_dim[1], n_pixel, 2), dtype=np.int16, fill_value=np.nan)
+    n_pixel = 25
+    Source = np.ones(shape=(hsi_dim[0], hsi_dim[1], n_pixel, 2), dtype=np.int16)*-1
     for i in range(rgb_dim[0]):
+        if i%10 == 0:
+            print(f"Row {i}")
         for j in range(rgb_dim[1]):
             Destcoord = transformation@np.array([i, j, 1])
             Dest[i,j,:] = np.array([int(Destcoord[0]),int(Destcoord[1])])
             if 0 <= int(Destcoord[0]) < hsi_dim[0] and 0 <= int(Destcoord[1]) < hsi_dim[1]:
                 k = 0
                 #print(int(Destcoord[0]),int(Destcoord[1]))
-                while Source[int(Destcoord[0]),int(Destcoord[1]),k,0] != -1:
+                while Source[int(Destcoord[0]), int(Destcoord[1]), k, 0] != -1:
                     k += 1
-                    assert k < (n_pixel), "Array size inadequate"
+                    assert k < n_pixel, "Array size inadequate"
                 Source[int(Destcoord[0]),int(Destcoord[1]),k,:] = np.array([i,j])
     
     return Source
 
 def generate_spatial_subset(area_of_interest, full_transform):
+    #need to implement method to correct values from each pixel, at the moment each pixel contributes equally
     subtransform = full_transform[area_of_interest[0]:area_of_interest[1],area_of_interest[2]:area_of_interest[3],:,:]
-    rgb_x = np.max(subtransform[:,:,:,0])-np.min(subtransform[:,:,:,0])
-    rgb_y = np.max(subtransform[:,:,:,1])-np.min(subtransform[:,:,:,1])
+    maskx = subtransform[:,:,:,0] != -1
+    masky = subtransform[:,:,:,1] != -1
+    print(f"np.max(x) = {np.max(subtransform[:,:,:,0])}, np.min(x) = {np.min(subtransform[:,:,:,0][maskx])}")
+    print(f"np.max(y) = {np.max(subtransform[:,:,:,1])}, np.min(y) = {np.min(subtransform[:,:,:,1][masky])}")
+    rgb_x_min, rgb_x_max = np.min(subtransform[:,:,:,0][maskx]), np.max(subtransform[:,:,:,0])
+    rgb_y_min, rgb_y_max = np.min(subtransform[:,:,:,1][masky]), np.max(subtransform[:,:,:,1])
+    rgb_x = rgb_x_max-rgb_x_min
+    rgb_y = rgb_y_max-rgb_y_min
     
     hsi_x = area_of_interest[1] - area_of_interest[0]
     hsi_y = area_of_interest[3] - area_of_interest[2]    
     
-    transform_array = np.zeros(shape=(rgb_x*rgb_y,hsi_x*hsi_y))
+    transform_array = np.zeros(shape=((rgb_x+1)*(rgb_y+1),hsi_x*hsi_y))
     
     for i in range(subtransform.shape[0]):
         for j in range(subtransform.shape[1]):
-            pass
+            k = 0
+            while subtransform[i,j,k,0] != -1:
+                try:
+                    transform_array[
+                        subtransform[i, j, k, 0] - rgb_x_min + (subtransform[i, j, k, 1] - rgb_y_min) * rgb_x,
+                        i + j * hsi_x
+                    ] = 1  # TODO: Implement weighting
 
+                except IndexError as e:
+                    raise IndexError(
+                        f"Out-of-bounds access in transform_array: "
+                        f"Index ({subtransform[i, j, k, 0] - rgb_x_min}, {subtransform[i, j, k, 1] - rgb_y_min}) \n"
+                        f"Transformed index: {subtransform[i, j, k, 0] - rgb_x_min + (subtransform[i, j, k, 1] - rgb_y_min) * rgb_x, i + j * hsi_x}\n"
+                        f"From: {subtransform[i, j, k, 0] - rgb_x_min} + {(subtransform[i, j, k, 1] - rgb_y_min)}*{rgb_x}\n"
+                        f"with shape {transform_array.shape}. Check index calculations."
+                    ) from e
+
+                except Exception as e:
+                    raise RuntimeError(f"Unexpected error in array indexing: {e}") from e
+
+                    
+                k += 1
+    
+    normalization_weights = np.sum(transform_array, axis=0)
+    transform_array = transform_array/normalization_weights
+    print(transform_array)
+    return transform_array
+
+def get_pixels(pixel_bounds, transformation, rgb_dim):
+    hsi_x_min, hsi_x_max = pixel_bounds[0], pixel_bounds[1]
+    hsi_y_min, hsi_y_max = pixel_bounds[2], pixel_bounds[3]
+    
+    hsi_x, hsi_y = hsi_x_max-hsi_x_min, hsi_y_max-hsi_y_min
+    
+    origin = transformation@np.array([hsi_x_min, hsi_y_min, 1.0])
+    extent = transformation@np.array([hsi_x_max, hsi_y_max, 1.0])
+    
+    rgb_x_min, rgb_x_max = int(np.floor(origin[0])), int(np.ceil(extent[0]))
+    rgb_y_min, rgb_y_max = int(np.floor(origin[1])), int(np.ceil(extent[1]))
+    
+    rgb_x, rgb_y = rgb_x_max-rgb_x_min, rgb_y_max-rgb_y_min
+    
+    hsi_pixels = hsi_x*hsi_y
+    rgb_pixels = rgb_x*rgb_y
+    
+    transform = np.zeros(shape=(rgb_pixels, hsi_pixels))
+    
+    for i in range(hsi_pixels):
+        x = i%hsi_x
+        y = i//hsi_x
+        area_origin = transformation@np.array([x+hsi_x_min, y+hsi_y_min, 1.0])
+        area_extent = transformation@np.array([x+1+hsi_x_min, y+1+hsi_y_min, 1.0])
+        x_min, x_max = int(np.floor(area_origin[0]))-rgb_x_min, int(np.ceil(area_extent[0]))-rgb_x_min
+        y_min, y_max = int(np.floor(area_origin[1]))-rgb_y_min, int(np.ceil(area_extent[1]))-rgb_y_min
+        for j in range(x_max-x_min):
+            for k in range(y_max-y_min):
+                transform[j+x_min+(k+y_min)*rgb_x,i] = 1.0
+                print(f"assigning link between RGB ({j+x_min},{k+y_min}) and HSI ({x},{y})")
+    
+    normalization_weights = np.sum(transform, axis=0)
+    transform = transform/normalization_weights
+    return transform
+    
+def full_transform(rgb_img, HSI_datacube):
+    rgb_mask = np.loadtxt("RGB_mask.txt")
+    spectral_response_matrix = util.map_mask_to_bands(rgb_mask[0:700,:],112)
+    hsi_img = np.matmul(HSI_datacube,spectral_response_matrix.T)
+    hsitorgb = align_and_overlay(hsi_img, rgb_img)
+    rgbtohsi = cv2.invert(hsitorgb)
+    origin = rgbtohsi@np.array([0, 0, 1.0])
+    extent = rgbtohsi@np.array([rgb_img.shape[0], rgb_img.shape[1], 1.0])
+    x_min = np.clip(origin[0], a_min=0.0, a_max=hsi_img.shape[0]) #this still assumes entire bounded square is defined
+    x_max = np.clip(extent[0], a_min=0.0, a_max=hsi_img.shape[0])
+    y_min = np.clip(origin[1], a_min=0.0, a_max=hsi_img.shape[1])
+    y_max = np.clip(extent[1], a_min=0.0, a_max=hsi_img.shape[1])
+    hsi_img = hsi_img[x_min:x_max, y_min:y_max]
+    transform = align_and_overlay(hsi_img, rgb_img) #lazy solution
+    active_area = np.array([x_min, x_max, y_min, y_max])
+    return active_area, transform
     
 if __name__ == "__main__":
-    hsi_path = "input/HSI.png"
-    rgb_path = "input/RGB.png"
+    #hsi_path, _ = util.Get_path("HSI image")
+    hsi_path = "slice.png"
+    #rgb_path, _ = util.Get_path("RGB image")
+    rgb_path = "data\\bluenile_2025_01_25T08_23_14.png"
+    hsi_img = cv2.normalize(cv2.imread(hsi_path, cv2.IMREAD_GRAYSCALE), None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    rgb_img = cv2.normalize(cv2.imread(rgb_path, cv2.IMREAD_GRAYSCALE), None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
     output_path = "output/"
     if not os.path.exists(output_path):
         os.makedirs(output_path)
-    transform = align_and_overlay(hsi_path=hsi_path, rgb_path=rgb_path, output_path=output_path)
-    Source = generate_full_spatial((4784,1092),(3840,2748), transform)
+    transform = align_and_overlay(hsi_img, rgb_img, output_path=output_path)
+    #transform_subset = get_pixels(np.array([500, 503, 500, 503]), transform, (0,0))
+    #print(transform_subset.shape)
+    

utilities.py
@@ -39,18 +39,18 @@ def band_to_wavelength(band: int):
     return (wavelengths[band],wavelengths[band+1])
     
 
-def Get_path():
+def Get_path(filetype: str):
         # Initialize Tkinter and hide the main window
         root = Tk()
         root.withdraw()
 
         # Open the file explorer and allow the user to select a file
-        file_path_input = askopenfilename(title="Select a file", initialdir=Path.cwd())
+        file_path_input = askopenfilename(title=f"Select {filetype} file", initialdir=Path.cwd())
 
         # Initialize a Path object
         file_path = Path(file_path_input)
         name = file_path.stem
-        return file_path, name
+        return file_path, name[:-4]
 
 def Get_subset(path, size, subset) -> np.array:
         flattened_array = np.loadtxt(fname=path)